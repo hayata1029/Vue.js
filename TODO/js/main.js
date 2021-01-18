6719979c15a62120f574954c4ba4889557d90129
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-18 19:49:27 +0900

    Create delete button in todo app

--- TODO/js/main.js
+++ TODO/js/main.js
@@ -9,10 +9,15 @@ var app = new Vue({
       // alert();
       if(this.newItem == '') return;
       var todo = {
-        item: this.newItem
+        item: this.newItem,
+        isDone: false
       };
       this.todos.push(todo);
       this.newItem = '';
+    },
+    deleteItem: function(index) {
+      // alert(index);
+      this.todos.splice(index, 1);
     }
   }
 })
