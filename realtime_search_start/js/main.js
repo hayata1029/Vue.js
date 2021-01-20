27ce884a3e3f5acb17e79ad62fbb6c042dab89d5
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-20 22:09:45 +0900

    Form Monitoring

--- realtime_search_start/js/main.js
+++ realtime_search_start/js/main.js
@@ -6,16 +6,22 @@ var app = new Vue({
       message: ''
     },
     watch: {
-
+      keyword:function(nerKeyword, oldKeyword) {
+        // console.log(nerKeyword)
+        this.message = 'Waiting for you to stop typing...'
+        this.debouncedGetAnswer()
+      }
     },
     created: function() {
-      this.keyword = 'JavaScript'
-      this.getAnswer()
+      // this.keyword = 'JavaScript'
+      // this.getAnswer()
+      this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
     },
     methods: {
       getAnswer: function() {
         if(this.keyword === '') {
-          this.item = null
+          this.items = null
+          this.message = ''
           return
         }
 
