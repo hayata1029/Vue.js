ad3e9e0fcf837552e1f8e7173697c559fec5fa10
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-24 11:56:52 +0900

    Create a number guessing game

--- Javascript/book-js/practice/3-04_conparison/script.js
+++ Javascript/book-js/practice/3-04_conparison/script.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const answer = window.prompt('ヘルプを見ますか？');
-if(answer === 'yes') {
-  window.alert('タップでジャンプ、障害物を避けます');
-} else if (answer === 'no') {
-  window.alert('ゲーム起動中');
+const number = Math.floor(Math.random() * 6 );
+const answer = parseInt(window.prompt('数当てゲーム。0〜５を入力してね'));
+let message;
+if (answer === number) {
+  message = 'あたり！';
+} else if (answer < number) {
+  message = 'もう少し大きい数字だよ';
+} else if (answer > number) {
+  message = 'もう少し小さい数字だよ';
 } else {
-  window.alert('yesかnoを入力してください')
+  message = '0~5までの数字を入力してください！！！'
 }
+
+window.alert(message);
